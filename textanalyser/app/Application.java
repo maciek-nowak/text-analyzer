75b1221b424a973a5972e7486b3b02bf769802ea
Maciej Nowak <mcnowak@wp.pl>
2017-09-11 11:55:44 +0200

    Implement Application class

---
+++ textanalyser/app/Application.java
@@ -0,0 +1,13 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.io.FileNotFoundException;
+
+import view.View;
+import dataaccess.FileContent;
+import analysis.StatisticalAnalysis;
+
+public class Application {
+    private View printer = new View();
+}
