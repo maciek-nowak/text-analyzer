ceb903bbf4b3aa09fa0719dcc9919e469d0585cb
Maciej Nowak <mcnowak@wp.pl>
2017-09-11 11:37:09 +0200

    Implement Statistical Analysis class with constructor

---
+++ textanalyser/analysis/StatisticalAnalysis.java
@@ -0,0 +1,35 @@
+package analysis;
+
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.Map;
+import java.util.Iterator;
+
+public class StatisticalAnalysis {
+    private LinkedHashMap<String, Integer> itemsOccurencies;
+
+    public StatisticalAnalysis(Iterator<String> iter) {
+        this.itemsOccurencies = new LinkedHashMap<>();
+        this.storeOccurencies(iter);
+    }
+
+    public Map<String, Integer> getItemsOccurencies() {
+        return this.itemsOccurencies;
+    }
+
+    private void storeOccurencies(Iterator<String> iter) {
+
+        while(iter.hasNext()) {
+            String element = iter.next();
+
+            if(this.itemsOccurencies.containsKey(element)) {
+                this.itemsOccurencies.put(element, itemsOccurencies.get(element) + 1);
+            } else {
+                this.itemsOccurencies.put(element, 1);
+            }
+
+        }
+
+    }
+}
