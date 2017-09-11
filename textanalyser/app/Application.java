f32434b67a35e0fc2dc7f54f4f9728f3300c1882
Maciej Nowak <mcnowak@wp.pl>
2017-09-11 13:19:29 +0200

    Implement testAndPrint method in Application class

--- textanalyser/app/Application.java
+++ textanalyser/app/Application.java
@@ -10,6 +10,37 @@ import analysis.StatisticalAnalysis;
 
 public class Application {
     private View printer = new View();
+
+    private void testAndPrint(FileContent fileContent) {
+        StatisticalAnalysis charAnalyzer = new StatisticalAnalysis(fileContent.charIterator());
+        StatisticalAnalysis wordAnalyzer = new StatisticalAnalysis(fileContent.wordIterator());
+
+        String title = String.format("==%s==", fileContent.getFilename());
+        printer.printMessage("\n" + title);
+
+        int charCount = charAnalyzer.size();
+        int wordCount = wordAnalyzer.size();
+
+        printer.printMessage("Char count: " + charCount);
+        printer.printMessage("Word count: " + wordCount);
+        printer.printMessage("Dict size: " + wordAnalyzer.dictionarySize());
+        printer.printMessage("Most used words (>1%): " + wordAnalyzer.occurMoreThan(wordCount / 100));
+
+        String[] wordsToFind = {"love", "hate", "music"};
+        for(String word : wordsToFind) {
+            printer.printMessage(String.format("'%s' count: %d", word, wordAnalyzer.countOf(word)));
+        }
+
+        int vowelsCount = charAnalyzer.countOf("A", "O", "E", "I", "U");
+        printer.printMessage("vowels %: " + (int) (vowelsCount / (double) charCount * 100));
+
+        double ratio = Math.round(charAnalyzer.countOf("A") / (double) charAnalyzer.countOf("E") * 100.0) / 100.0;
+        printer.printMessage("'a':'e' count ratio: " + ratio);
+
+        Map<String, Integer> charsOccurencies = charAnalyzer.getItemsOccurencies();
+        printer.printMap(charsOccurencies, charCount);
+    }
+
     private void start(String[] args) {
 
         for(String arg: args) {
@@ -40,4 +71,5 @@ public class Application {
         new View().printMessage("Benchmark time: " + elapsedTime + " secs");
 
     }
+
 }
